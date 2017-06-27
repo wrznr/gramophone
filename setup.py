b3413e18019238eaf60fa92445d996bcf1df526e
Kay-Michael Würzner <wuerzner@bbaw.de>
2017-06-27 14:03:30 +0200

    Intial project structure

---
+++ setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='gramophone',
+    version='0.0.1',
+    description='grapheme-phoneme conversion and related stuff',
+    long_description=readme,
+    author='Kay-Michael Würzner, Bryan Jurish',
+    author_email='wuerzner@bbaw.de, jurish@bbaw.de',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=[
+    ],
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+)
