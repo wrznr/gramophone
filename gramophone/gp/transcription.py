90119e7ec16ac8e548b3b9bb59847566b6313536
Kay-Michael Würzner <wuerzner@bbaw.de>
2017-06-30 15:25:21 +0200

    module for transcription stage

---
+++ gramophone/gp/transcription.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+import wapiti
+
+class Transcriber:
+
+    def __init__(self):
+        '''
+        The constructor.
+        '''
+
+        self.clear()
+
+    def clear(self):
+        '''
+        Resets transcriber.
+        '''
+
+        self.model = wapiti.Model(patterns="U1+0:%x[0,0]",nbest=2)
+        self.status = 0
+
+
+    def train(self, training_data, model_file):
+        '''
+        Trains a crf model.
+        '''
+        
+        # parse training data
+        for alignment in training_data:
+            seq = u"\n".join(u"%s %s" % (x,y) for x,y in zip(alignment[0],alignment[1]))
+            self.model.add_training_sequence(seq)
+
+        # training step
+        self.model.train()
+
+        self.status = 1
+
+    def load(self,model_file):
+        '''
+        Loads a previously trained model.
+        '''
+        self.clear()
+        self.model = wapiti.Model(model=model_file)
+        self.status = 1
+
+    def transcribe(self,graphemes):
+        if self.status > 0:
+            return self.model.label_sequence(u"\n".join(graphemes)).decode("utf-8").strip().split(u"\n")
+        else:
+            return []
