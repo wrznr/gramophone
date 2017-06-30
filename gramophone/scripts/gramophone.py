0f529c7e5e61509552fda2ca21a5eca7f445fd11
Kay-Michael Würzner <wuerzner@bbaw.de>
2017-06-30 10:52:00 +0200

    CLI training mode: added alignment

--- gramophone/scripts/gramophone.py
+++ gramophone/scripts/gramophone.py
@@ -1,14 +1,56 @@
 import click
 
+from gramophone import gp
+
 @click.group()
 def cli():
     pass
 
 @click.command()
-@click.option('--model', default=1, help='number of greetings')
+@click.option('-M', '--mapping', required=True, help='grapheme-phoneme mapping')
+@click.option('-m', '--model', default='model', help='prefix of the output model files')
 @click.argument('data')
-def train():
+def train(mapping,model,data):
     """Train a model"""
-    click.echo('Hello World!')
+
+    #
+    # stage 1: alignment
+    #
+    click.echo(u"Stage 1: training data alignment")
+
+    # the aligner
+    aligner = gp.Aligner(mapping=mapping)
+
+    # iterate over input and align training data
+    aligned_training_data = []
+    with open(str(data),"r") as f:
+        training_data = f.read()
+        for line in training_data.split(u"\n"):
+
+            # skip comments
+            if line.startswith(u"#"):
+                continue
+
+            # assume tab-separated values
+            fields = line.split(u"\t")
+            if len(fields) < 2:
+                continue
+
+            # align
+            alignment = aligner.align(fields[0],fields[1])
+            aligned_training_data.append(alignment)
+
+    click.echo(aligned_training_data)
+
+    #
+    # stage 2: crf training
+    #
+
+    #
+    # stage 3: language model training
+    #
+
+    
+            
 
 cli.add_command(train)
